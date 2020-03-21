0bfc9892616dff16625033358c01fe873af7a593
Maksim <maksisbelarus01@gmail.com>
2020-03-21 23:57:21 +0300

    feat: delete commets

--- script.js
+++ script.js
@@ -15,40 +15,40 @@ const hovermenubar = (event) => {
 }
 document.getElementById('list').addEventListener("click",hovermenubar)
 
-// const hoverimg = (event) => {
-//     document.querySelectorAll('img').forEach(img=>img.style.border="")
-//     event.target.style.border = "5px solid #F06C64";
-// }
-// document.getElementById('imgs').addEventListener("click",hoverimg)
+const hoverimg = (event) => {
+    document.querySelectorAll('img').forEach(img=>img.style.border="")
+    event.target.style.border = "5px solid #F06C64";
+}
+document.getElementById('imgs').addEventListener("click",hoverimg)
 
-// document.querySelector('button').addEventListener("click",()=>{
-//     event.preventDefault();
-//     let form = document.forms.form;
-//     let elemName = form.elements.name; 
-//     console.log(elemName.value);
-//     let elemEmail = form.elements.email;
-//     console.log(elemEmail.value)
-//     let elemSubject = form.elements.subject;
-//     console.log(elemSubject.value);
-//     document.getElementById('block-form').style.display="block";
-//     document.getElementById('block-content').style.display="block"; })
+document.querySelector('button').addEventListener("click",()=>{
+    event.preventDefault();
+    let form = document.forms.form;
+    let elemName = form.elements.name; 
+    console.log(elemName.value);
+    let elemEmail = form.elements.email;
+    console.log(elemEmail.value)
+    let elemSubject = form.elements.subject;
+    console.log(elemSubject.value);
+    document.getElementById('block-form').style.display="block";
+    document.getElementById('block-content').style.display="block"; })
 
-//  document.getElementById('button-Ok').addEventListener("click",() => {
-//     event.preventDefault();
-//     document.getElementById('block-form').style.display="none";
-//     document.getElementById('block-content').style.display="none";
-//  })
-//  function clickOnIphone(){
-//     let style_for_screen = 'background: #000';
-//     let this_iphone = Array.from(this.getElementsByClassName('sleep'))[0];
-//     if(this_iphone.style.length){
-//         this_iphone.style = '';
-//     }else{
-//         this_iphone.style = style_for_screen;
-//     }
-// }
-// let iphone = Array.from(document.getElementsByClassName('body'));
-// iphone.forEach(elem => elem.onclick = clickOnIphone);
+ document.getElementById('button-Ok').addEventListener("click",() => {
+    event.preventDefault();
+    document.getElementById('block-form').style.display="none";
+    document.getElementById('block-content').style.display="none";
+ })
+ function clickOnIphone(){
+    let style_for_screen = 'background: #000';
+    let this_iphone = Array.from(this.getElementsByClassName('sleep'))[0];
+    if(this_iphone.style.length){
+        this_iphone.style = '';
+    }else{
+        this_iphone.style = style_for_screen;
+    }
+}
+let iphone = Array.from(document.getElementsByClassName('body'));
+iphone.forEach(elem => elem.onclick = clickOnIphone);
 document.addEventListener('scroll',onScroll);
 function onScroll(){
     let curPos= window.scrollY;
