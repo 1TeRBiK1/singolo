8766a7fad58921a7bfc324c0586ca54359d89b13
1TeRBiK1 <maksisbelarus01@gmail.com>
2022-11-07 01:15:42 +0300

    add fullscreen color and fix js functions

--- script.js
+++ script.js
@@ -1,94 +1,123 @@
-const hovermenu = (event) => {
-    document.querySelectorAll('a').forEach(a=>a.style.color="")
-    event.target.style.color="#f06c64";
+const portfolioImgButtons = document.querySelectorAll("#listButton > li");
+
+const onClickToPortfolioImgButton = (event) => {
+  portfolioImgButtons.forEach((buttons) => {
+    buttons.style.color = "";
+  });
+
+  event.target.style.color = "#ffffff";
+
+  const scrollpicture = document.querySelector("#imgs > img:nth-child(1)");
+  const pictureclone = scrollpicture.cloneNode(true);
+
+  scrollpicture.remove();
+  document.querySelector("#imgs").appendChild(pictureclone);
+};
+
+portfolioImgButtons.forEach((button) =>
+  button.addEventListener("click", onClickToPortfolioImgButton)
+);
+
+const onClickToPortfolioImg = (event) => {
+  const newBorderImg = "5px solid rgb(240, 108, 100)";
+  console.log(event.target, event.target.style.border === newBorderImg);
+  event.target.style.border === newBorderImg
+    ? (event.target.style.border = "")
+    : (event.target.style.border = newBorderImg);
+  document
+    .querySelectorAll(".img-hide13")
+    .forEach((img) => (img === event.target ? null : (img.style.border = "")));
+};
+
+const portfolioImgs = document.getElementById("imgs");
+portfolioImgs.addEventListener("click", onClickToPortfolioImg);
+
+const enableModal = (event) => {
+  event.preventDefault();
+  document.getElementById("block-form").style.display = "block";
+  document.getElementById("block-content").style.display = "block";
+};
+
+const onClickToSubmitButton = (event) => {
+  event.preventDefault();
+  const form = document.forms.form;
+
+  const elemdescribtion = form.elements.describtion;
+  const elemSubject = form.elements.subject;
+
+  const subject_message =
+    elemSubject.value == "" ? "Без темы" : "Тема: " + elemSubject.value;
+
+  const describtion_message =
+    elemdescribtion.value == ""
+      ? "Без описания"
+      : "Описание: " + elemdescribtion.value;
+
+  document.querySelector(".subject-mes").innerText = `${subject_message}`;
+  document.querySelector(".describe-mes").innerText = `${describtion_message}`;
+
+  enableModal(event);
+};
+
+const submitButtton = document.getElementById("#submit");
+submitButtton.addEventListener("click", onClickToSubmitButton);
+
+const disableModal = (event) => {
+  event.preventDefault();
+  document.getElementById("block-form").style.display = "none";
+  document.getElementById("block-content").style.display = "none";
+};
+
+const modalButton = document.getElementById("modalButton");
+modalButton.addEventListener("click", disableModal);
+
+function clickOnIphone() {
+  const style_for_screen = "background: #000";
+  const this_iphone = this.getElementsByClassName("sleep")[0];
+  this_iphone.style.background
+    ? (this_iphone.style = "")
+    : (this_iphone.style = style_for_screen);
 }
-document.querySelector('ul').addEventListener("click",hovermenu)
-const hovermenubar = (event) => {
-    document.querySelectorAll('a').forEach(a=>a.style.color="")
-    event.target.style.color="#c5c5c5";
-    document.querySelectorAll('li').forEach(li=>li.style.borderColor="")
-    event.target.parentElement.style.borderColor="#c5c5c5";
-    let scrollpicture =document.querySelector("#imgs > img:nth-child(1)")
-    let pictureclone = scrollpicture.cloneNode(true);
-    document.querySelector("#imgs").appendChild(pictureclone);
-    scrollpicture.remove();
-}
-document.getElementById('list').addEventListener("click",hovermenubar)
 
-const hoverimg = (event) => {
-    document.querySelectorAll('img').forEach(img=>img.style.border="")
-    event.target.style.border = "5px solid #F06C64";
-}
-document.getElementById('imgs').addEventListener("click",hoverimg)
+const iphones = document.querySelectorAll(".iphone");
+iphones.forEach((iphone) => iphone.addEventListener("click", clickOnIphone));
 
-document.querySelector('button').addEventListener("click",()=>{
-    event.preventDefault();
-    let form = document.forms.form;
-    let elemsubject = form.elements.subject; 
-    let elemdescribtion = form.elements.describtion;
-    let elemSubject = form.elements.subject;
-    let subject_message = elemsubject.value == '' ? 'Без темы' : 'Тема: ' + elemsubject.value;
-    let describtion_message = elemdescribtion.value == '' ? 'Без описания' : 'Описание: ' + elemdescribtion.value;
-    document.querySelector('.subject-mes').innerText = `${subject_message}`;
-    document.querySelector('.describe-mes').innerText = `${describtion_message}`;
-
-    console.log(elemSubject.value);
-    document.getElementById('block-form').style.display="block";
-    document.getElementById('block-content').style.display="block"; })
-
- document.getElementById('button-Ok').addEventListener("click",() => {
-    event.preventDefault();
-    document.getElementById('block-form').style.display="none";
-    document.getElementById('block-content').style.display="none";
- })
- function clickOnIphone(){
-    let style_for_screen = 'background: #000';
-    let this_iphone = Array.from(this.getElementsByClassName('sleep'))[0];
-    if(this_iphone.style.length){
-        this_iphone.style = '';
-    }else{
-        this_iphone.style = style_for_screen;
+const onScroll = (event) => {
+  const sectionDivs = document.querySelectorAll("body > div");
+  const navigationLink = document.querySelectorAll(".hatlist > li > a");
+  const height = event.pageY;
+  sectionDivs.forEach((section, index) => {
+    if (section.offsetTop <= height) {
+      navigationLink.forEach((link) => (link.style.color = "#ffffff"));
+      navigationLink[index].style.color = "#f06c64";
     }
-}
-let iphone = Array.from(document.getElementsByClassName('body'));
-iphone.forEach(elem => elem.onclick = clickOnIphone);
-document.addEventListener('scroll',onScroll);
-function onScroll(){
-    let curPos= window.scrollY;
-    let divs = document.querySelectorAll('body>div');
-    let navigation = document.querySelectorAll('li>a');
-    divs.forEach((el)=>{
-        if(el.offsetTop <= curPos){
-            navigation.forEach((a)=>{
-                a.style.color="#ffffff";
-                if(a.getAttribute('href').substring(1) === el.getAttribute('id')){
-                    a.style.color="#f06c64";   
-}
-            })
-        }
-    });
-}
-function clickOnChev(){
-    if(slider_1){
-        center.forEach(elem => elem.style = 'display: none');
-     //   center.style = 'background: url(assets/slider-2.png);height: 513px; ';
-        slider.style = 'background-color: #648bf0; border-bottom: 6px solid #74b9ff;background: url(assets/slider-2.png);background-repeat: no-repeat;';
-        left_button.style = 'right: -4px; top: 2px;';
-        slider_1 = false;
-    }else{
-        center.style = '';
-        center.forEach(elem => elem.style = '');
-        slider.style = 'background-color: #f06c64; border-bottom: 6px solid #ea676b;';
-        slider_1 = true;
-        left_button.style = '';
-    
-    }
-}
-let left_button = Array.from(document.getElementsByClassName('left-button'))[0];
-let center = Array.from(document.getElementsByClassName('center'));
-let slider = Array.from(document.getElementsByClassName('slider'))[0];
+  });
+};
 
+document.addEventListener("mousemove", onScroll);
+
+function clickOnChev() {
+  if (slider_1) {
+    center.forEach((elem) => (elem.style = "display: none"));
+    //   center.style = 'background: url(assets/slider-2.png);height: 513px; ';
+    slider.style =
+      "background-color: #648bf0;background: url(assets/slider-2.png);background-repeat: no-repeat;";
+    left_button.style = "right: 416px; top: -2px;";
+    slider_1 = false;
+  } else {
+    center.style = "";
+    center.forEach((elem) => (elem.style = ""));
+    slider.style = "background-color: #f06c64";
+    slider_1 = true;
+    left_button.style = "";
+  }
+}
+const chevButtons = document.querySelectorAll(".chevButton");
+chevButtons.forEach((button) => button.addEventListener("click", clickOnChev));
+const left_button = [...document.getElementsByClassName("left-button")][0];
+
+const center = [...document.getElementsByClassName("center")];
+const slider = [...document.getElementsByClassName("slider")][0];
 let slider_1 = true;
-
-let subject = document.querySelector('#f-subject');
-let discribe = document.querySelector('#textarea');
+const subject = document.querySelector("#f-subject");
+const discribe = document.querySelector("#textarea");
